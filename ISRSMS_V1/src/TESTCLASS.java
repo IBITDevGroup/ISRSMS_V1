1c3c0c2874bbc50c1250c33a73d96153ff807ed3
BuildTools <unconfigured@null.spigotmc.org>
2015-01-29 22:00:39 +0100

    First change check

--- ISRSMS_V1/src/TESTCLASS.java
+++ ISRSMS_V1/src/TESTCLASS.java
@@ -4,6 +4,7 @@ public class TESTCLASS {
 	public static void main(String[] args) {
 		
 		System.out.println("This class is for testing of GIT only!!");
+		System.out.println("Checking first change");
 
 	}
 
