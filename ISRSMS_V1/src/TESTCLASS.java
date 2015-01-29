455b99187a47e319b2533467c22fd5e0bb818bad
Tim herkenrath <timherken@gmail.com>
2015-01-29 22:06:58 +0100

    Added comments!

--- ISRSMS_V1/src/TESTCLASS.java
+++ ISRSMS_V1/src/TESTCLASS.java
@@ -2,6 +2,8 @@
 public class TESTCLASS {
 
 	public static void main(String[] args) {
+		//This is the git check file#
+		//Add anything if you want to check git's functions
 		
 		System.out.println("This class is for testing of GIT only!!");
 		System.out.println("Checking first change");
