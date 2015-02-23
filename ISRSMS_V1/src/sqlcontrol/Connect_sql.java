b45cbf12364755027415ed45610757ab3354d8a4
malte12g <malte.granderath@t-online.de>
2015-02-23 11:52:55 +0100

    Basic Connection checker for sql database

--- ISRSMS_V1/src/sqlcontrol/Connect_sql.java
+++ ISRSMS_V1/src/sqlcontrol/Connect_sql.java
@@ -1,5 +1,37 @@
 package sqlcontrol;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Connect_sql {
+	
+	 public static void main(String[] args) {
+		 Connection conn = null;
+	try {
+        // The newInstance() call is a work around for some
+        // broken Java implementations
 
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+    } catch (Exception ex) {
+        System.out.println("Error");
+    }
+	try {
+	    conn =
+	       DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+	                                   "user=monty&password=greatsqldb");
+
+	    // Do something with the Connection
+
+	  
+	} catch (SQLException ex) {
+	    // handle any errors
+	    System.out.println("SQLException: " + ex.getMessage());
+	    System.out.println("SQLState: " + ex.getSQLState());
+	    System.out.println("VendorError: " + ex.getErrorCode());
+	}
+	
+	
+	
+	 }
+	 
 }
